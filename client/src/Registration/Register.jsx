7383511d1994f4d08029d2f30147f0c9f3bef86b
Sahil <garg07825@gmail.com>
2023-12-24 13:58:00 +0530

    Register page design is ready

--- client/src/Registration/Register.jsx
+++ client/src/Registration/Register.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          type="text"
+          placeholder="username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
