20961512c0a4f9cb769fb732fb929af9c3072881
Sahil <garg07825@gmail.com>
2023-12-24 14:38:08 +0530

    one route is created for register

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import { config } from "dotenv";
 import bodyParser from "body-parser";
-
+import User from "./models/User.js";
+import jwt from "jsonwebtoken";
 export const app = express();
 config({
   path: "./.env",
 });
-
+const jwtSecret = process.env.JWT_Secret;
 // routes
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post('/register', (req, res) => {
-    
-})
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.create({ username, password });
+    await jwt.sign({ userId: user._id }, jwtSecret).then((token) => {
+      res.cookie("token", token).status(201).json("ok");
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 // middlewares
 app.use(bodyParser.json());
