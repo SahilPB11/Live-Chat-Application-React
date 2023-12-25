eade353d936b695c8e749b194cae3b818157708b
Sahil <garg07825@gmail.com>
2023-12-25 19:16:02 +0530

    now we can select the user in left bar

--- client/src/component/Chat.jsx
+++ client/src/component/Chat.jsx
@@ -4,6 +4,7 @@ import Avatar from "./Avatar";
 const Chat = () => {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
   const url = import.meta.env.VITE_APP_WS_SERVER_URL;
   useEffect(() => {
     const ws = new WebSocket(`ws://${url}`);
@@ -24,10 +25,15 @@ const Chat = () => {
       showOnlinePeople(messageData?.online);
     }
   }
+  // for apitalize the first letter of user
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   return (
     <div className="flex h-screen">
-      <div className=" bg-white w-2/6 pl-4 pt-4">
-        <div className="text-blue-600 font-bold flex gap-2 mb-4">
+      <div className=" bg-white w-2/6  pt-4">
+        <div className="text-blue-600 font-bold flex gap-2 p-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -37,16 +43,25 @@ const Chat = () => {
             <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
             <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
           </svg>
-          Mern Chat
+          MernChat
         </div>
         {Object.keys(onlinePeople)?.map((userId) => (
-          <div className="border-b border-gray-100 py-2 flex items-center gap-2" key={userId}>
+          <div
+            className={`border-b border-gray-100 pl-2 py-2 flex items-center gap-2 cursor-pointer ${
+              selectedUserId === userId ? "bg-blue-50" : ""
+            }`}
+            key={userId}
+            onClick={() => setSelectedUserId(userId)}
+          >
             <Avatar userId={userId} username={onlinePeople[userId]} />
-            <span> {onlinePeople[userId]}</span>
+            <span className="text-gray-800">
+              {" "}
+              {capitalizeFirstLetter(onlinePeople[userId])}
+            </span>
           </div>
         ))}
       </div>
-      <div className="flex flex-col bg-blue-200 w-5/6 p-1">
+      <div className="flex flex-col bg-blue-100 w-5/6 p-1">
         <div className="flex-grow">messages with selected person </div>
         <div className="flex gap-1">
           <input
