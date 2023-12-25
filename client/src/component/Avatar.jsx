a90ca21b5853ab4531bf02c983cc176464a48a63
Sahil <garg07825@gmail.com>
2023-12-25 19:01:41 +0530

    Avatar component is ready

--- client/src/component/Avatar.jsx
+++ client/src/component/Avatar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Avatar = ({ userId, username }) => {
+  const colors = [
+    " bg-purple-200",
+    "bg-green-200",
+    "bg-orange-200",
+    "bg-blue-200",
+    "bg-gray-200",
+    "bg-red-200",
+    "bg-pink-200",
+    "bg-fuchsia-200",
+    "bg-teal-200",
+  ];
+  const userIdBase10 = parseInt(userId, 16);
+  const colorIndex = userIdBase10 % colors.length;
+  const color = colors[colorIndex];
   return (
-    <div className="w-8 h-8 bg-red-200 rounded-full flex items-center">
-      <div className="text-center w-full">{username[0]}</div>
+    <div className={`w-10 h-10 ${color} rounded-full flex items-center`}>
+      <div className="text-center w-full opacity-55 text-xl">{username[0]}</div>
     </div>
   );
 };
